3c7b51167aeb368f0a10bf4c3ebca75bbec3afc1
matei <mateimurarasu@yahoo.com>
2026-01-15 07:57:58 +0200

    update project

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,4 +1,5 @@
-"use client";
+export const dynamic = "force-dynamic";
+("use client");
 
 import { useState, useEffect, Suspense } from "react"; // Adăugat Suspense
 import { signIn } from "next-auth/react";
